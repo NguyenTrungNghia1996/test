26a899d366db78b0962c8c9a1aea8bcab5cc7979
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2025-04-04 09:41:20 +0700

    update test

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,74 +1,86 @@
 <template>
-  <a-layout>
-    <a-layout-header class="header">
-      <a-button v-if="isMobile" @click="toggleSider" class="menu-button">☰</a-button>
-    </a-layout-header>
-    
-    <a-layout>
-      <a-layout-content>
-        <slot />
-      </a-layout-content>
-      
-      <a-layout-sider v-if="isMobile" v-show="!collapsed" width="250" class="sider" placement="right">
-        <a-menu mode="inline" theme="light" :selectedKeys="[selectedKey]">
-          <a-menu-item key="1">Giới thiệu</a-menu-item>
-          <a-sub-menu key="2">
-            <template #title>Khóa học</template>
-            <a-menu-item key="2-1">Khóa học A</a-menu-item>
-            <a-menu-item key="2-2">Khóa học B</a-menu-item>
-          </a-sub-menu>
-          <a-menu-item key="3">Cuộc thi</a-menu-item>
-          <a-menu-item key="4">Văn bản</a-menu-item>
-          <a-menu-item key="5">Bài viết</a-menu-item>
-        </a-menu>
-      </a-layout-sider>
-    </a-layout>
-  </a-layout>
+  <div>
+    <a-menu mode="horizontal" theme="light" :selectedKeys="[selectedKey]">
+      <span>
+        <a-menu-item v-for="m in menu" :key="m.key" @click="handleClick(m.key)">
+          {{ m.title }}
+        </a-menu-item>
+      </span>
+
+      <!-- <a-menu-item key="1">Giới thiệu</a-menu-item>
+      <a-sub-menu key="2">
+        <template #title>Khóa học</template>
+<a-menu-item key="2-1">Khóa học A</a-menu-item>
+<a-sub-menu key="2-2">
+  <template #title> Khóa học B</template>
+  <a-menu-item key="2-2-1-1" @click="handleClick('2-2-1-1')">Khóa học B1.1</a-menu-item>
+  <a-menu-item key="2-2-1-2">Khóa học B1.2</a-menu-item>
+</a-sub-menu>
+</a-sub-menu>
+<a-menu-item key="3">Cuộc thi</a-menu-item>
+<a-menu-item key="4">Văn bản</a-menu-item>
+<a-menu-item key="5">Bài viết</a-menu-item> -->
+    </a-menu>
+
+    {{ selectedKey }}
+  </div>
 </template>
 
 <script setup>
 definePageMeta({ layout: "free" });
-import { ref, onMounted } from 'vue';
-
-const isMobile = ref(false);
-const collapsed = ref(true);
-const selectedKey = ref("1");
-
-const checkScreenSize = () => {
-  isMobile.value = window.innerWidth < 768;
-  collapsed.value = true;
-};
-
-const toggleSider = () => {
-  collapsed.value = !collapsed.value;
-};
-
-onMounted(() => {
-  checkScreenSize();
-  window.addEventListener('resize', checkScreenSize);
-});
+const selectedKey = ref("");
+const handleClick = (e) => {
+  selectedKey.value = e;
+}
+const menu = ref([
+  {
+    key: "1",
+    title: "Giới thiệu",
+    url: "/test/intro",
+  },
+  {
+    key: "2",
+    title: "Khóa học",
+    url: "/test/course",
+    child: [
+      {
+        key: "2-1",
+        title: "Khóa học A",
+        url: "/test/course/course-a",
+      },
+      {
+        key: "2-2",
+        title: "Khóa học B",
+        url: "/test/course/course-b",
+        child: [
+          {
+            key: "2-2-1-1",
+            title: "Khóa học B1.1",
+            url: "/test/course/course-b/course-b1-1",
+          },
+          {
+            key: "2-2-1-2",
+            title: "Khóa học B1.2",
+            url: "/test/course/course-b/course-b1-2",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: "3",
+    title: "Cuộc thi",
+    url: "/test/contest",
+  },
+  {
+    key: "4",
+    title: "Văn bản",
+    url: "/test/document",
+  },
+  {
+    key: "5",
+    title: "Bài viết",
+    url: "/test/article",
+  },
+]);
 </script>
-
-<!-- <style scoped>
-.sider {
-  height: 100vh;
-  background: #fff;
-  box-shadow: -2px 0 8px rgba(0, 0, 0, 0.1);
-  transition: width 0.3s, opacity 0.3s;
-  right: 0;
-}
-.header {
-  display: flex;
-  align-items: center;
-  padding: 0 20px;
-  background: #fff;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-}
-.menu-button {
-  font-size: 20px;
-  background: none;
-  border: none;
-  cursor: pointer;
-  margin-left: auto;
-}
-</style> -->
